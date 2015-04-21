69afe7e256302b1c66d30ebc9658979a78244658
Zhiw <karlkb81@gmail.com>
2015-04-22 00:22:33 +0800

    improve UI

--- app/src/main/java/com/zhiw/coolweather/app/activity/WeatherActivity.java
+++ app/src/main/java/com/zhiw/coolweather/app/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.zhiw.coolweather.app.activity;
 
 
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -63,8 +64,8 @@ public class WeatherActivity extends ActionBarActivity implements View.OnClickLi
         temp1Text = (TextView) findViewById(R.id.temp1);
         temp2Text = (TextView) findViewById(R.id.temp2);
         currentDateText = (TextView) findViewById(R.id.current_date);
-        switchCity = (Button) findViewById(R.id.switch_city);
-        refreshWeather = (Button) findViewById(R.id.refresh_weather);
+        //switchCity = (Button) findViewById(R.id.switch_city);
+        //refreshWeather = (Button) findViewById(R.id.refresh_weather);
         String countyCode = getIntent().getStringExtra("county_code");
         if (!TextUtils.isEmpty(countyCode)) {
 // 有县级代号时就去查询天气
@@ -76,14 +77,15 @@ public class WeatherActivity extends ActionBarActivity implements View.OnClickLi
 // 没有县级代号时就直接显示本地天气
             showWeather();
         }
-        switchCity.setOnClickListener(this);
-        refreshWeather.setOnClickListener(this);
+        //switchCity.setOnClickListener(this);
+        //refreshWeather.setOnClickListener(this);
     }
-    @Override
+
+    /*@Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.switch_city:
-                Intent intent = new Intent(this,ChooseAreaActivity.class);
+                Intent intent = new Intent(this, ChooseAreaActivity.class);
                 intent.putExtra("from_weather_activity", true);
                 startActivity(intent);
                 finish();
@@ -91,8 +93,8 @@ public class WeatherActivity extends ActionBarActivity implements View.OnClickLi
             case R.id.refresh_weather:
                 publishText.setText("同步中...");
                 SharedPreferences spfs = PreferenceManager.getDefaultSharedPreferences(this);
-                String weatherCode = spfs.getString("weather_code","");
-                if(!TextUtils.isEmpty(weatherCode)){
+                String weatherCode = spfs.getString("weather_code", "");
+                if (!TextUtils.isEmpty(weatherCode)) {
                     queryWeatherInfo(weatherCode);
                 }
                 break;
@@ -100,25 +102,26 @@ public class WeatherActivity extends ActionBarActivity implements View.OnClickLi
                 break;
         }
 
-    }
+    }*/
 
     /**
      * 查询县级代号所对应的天气
      */
-    private void queryWeatherCode(String countyCode){
-        String address =  "http://www.weather.com.cn/data/list3/city" + countyCode + ".xml";
-        queryFromServer(address,"countyCode");
+    private void queryWeatherCode(String countyCode) {
+        String address = "http://www.weather.com.cn/data/list3/city" + countyCode + ".xml";
+        queryFromServer(address, "countyCode");
     }
+
     /**
      * 查询天气代号对应的天气信息
      */
-    public void queryWeatherInfo(String weatherCode){
+    public void queryWeatherInfo(String weatherCode) {
         String address = "http://www.weather.com.cn/data/cityinfo/" + weatherCode + ".html";
         queryFromServer(address, "weatherCode");
 
     }
 
-    private void queryFromServer(final String address,final String type){
+    private void queryFromServer(final String address, final String type) {
         HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
             @Override
             public void onFinish(String response) {
@@ -158,12 +161,12 @@ public class WeatherActivity extends ActionBarActivity implements View.OnClickLi
     }
 
     /**
-     *从SharedPreferences文件中读取存储的天气信息，并显示到界面上。
+     * 从SharedPreferences文件中读取存储的天气信息，并显示到界面上。
      */
     private void showWeather() {
         SharedPreferences prefs = PreferenceManager.
                 getDefaultSharedPreferences(this);
-        cityNameText.setText( prefs.getString("city_name", ""));
+        cityNameText.setText(prefs.getString("city_name", ""));
         temp1Text.setText(prefs.getString("temp1", ""));
         temp2Text.setText(prefs.getString("temp2", ""));
         weatherDespText.setText(prefs.getString("weather_desp", ""));
@@ -190,12 +193,35 @@ public class WeatherActivity extends ActionBarActivity implements View.OnClickLi
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_refresh:
+                publishText.setText("同步中...");
+                SharedPreferences spfs = PreferenceManager.getDefaultSharedPreferences(this);
+                String weatherCode = spfs.getString("weather_code", "");
+                if (!TextUtils.isEmpty(weatherCode)) {
+                    queryWeatherInfo(weatherCode);
+                }
+                break;
+            case R.id.action_home:
+                Intent intent = new Intent(this, ChooseAreaActivity.class);
+                intent.putExtra("from_weather_activity", true);
+                startActivity(intent);
+                finish();
+                break;
+            default:
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
 
+    }
 }
+
+
+
+
